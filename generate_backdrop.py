7ec93996220eb4c939e26399ddd1a5aa81f7dbb2
chrisholmes02 <chris.holmes02@gmail.com>
2026-06-27 20:04:46 -0500

    update

--- generate_backdrop.py
+++ generate_backdrop.py
@@ -37,7 +37,7 @@ except ImportError:
 # ---------------------------------------------------------------------------
 GENERATOR_URL   = "https://paytonjewell.github.io/Nuvio-Backdrop-Generator/"
 GITHUB_TOKEN    = os.environ.get("GITHUB_TOKEN", "")
-GITHUB_REPO     = "chrisholmes02/TVImages" # os.environ.get("GITHUB_REPO", "")            # owner/repo
+GITHUB_REPO     = "chrisholmes02/TVImages"
 GITHUB_BRANCH   = os.environ.get("GITHUB_BRANCH", "main")
 GITHUB_FILE     = "Backdrops/"
 TMDB_KEY        = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlY2RkZjFjNzk4ZGUzYTRjNzk1NGViOTRkM2FkODY3ZCIsIm5iZiI6MTc3MDc3MTQwNi4wMDE5OTk5LCJzdWIiOiI2OThiZDNjZDJhMWM2MTI2ZTc4ODVjODgiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.EnCCVp3ieKgmi5hFEavsPkDBfA2_e7gI2iAuLSJYYG0"
