57dd1d0e8bfa23f85991a4c1bf5662fc35ee0eaa
chrisholmes02 <chris.holmes02@gmail.com>
2026-06-27 19:23:51 -0500

    h

--- generate_backdrop.py
+++ generate_backdrop.py
@@ -36,7 +36,7 @@ except ImportError:
 # Configuration (read from environment with sane defaults)
 # ---------------------------------------------------------------------------
 GENERATOR_URL   = "https://paytonjewell.github.io/Nuvio-Backdrop-Generator/"
-GITHUB_TOKEN    = "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" #os.environ.get("GITHUB_TOKEN", "")
+GITHUB_TOKEN    = os.environ.get("GITHUB_TOKEN", "")
 GITHUB_REPO     = "chrisholmes02/TVImages" # os.environ.get("GITHUB_REPO", "")            # owner/repo
 GITHUB_BRANCH   = os.environ.get("GITHUB_BRANCH", "main")
 GITHUB_FILE     = "Backdrops/"
@@ -49,9 +49,6 @@ bytes           = b""
 def generate_backdrops():
     global png_bytes, bytes, LOCAL_OUTPUT
 
-    ####
-    print(f"Repo {GITHUB_REPO} / Branch {GITHUB_BRANCH}")
-    
     print("1. Opening Chromium browser in headless session")
     
     with sync_playwright() as p:
