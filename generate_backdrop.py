65e1bbfe8aba8b06b34e79120c4620eeb38621a7
chrisholmes02 <chris.holmes02@gmail.com>
2026-06-30 17:50:10 -0500

    Update generate_backdrop.py

--- generate_backdrop.py
+++ generate_backdrop.py
@@ -45,7 +45,7 @@ def generate_backdrops():
 
     # Open a Playwright browser and navigate to the generator URL    
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         context = browser.new_context()
         page = context.new_page()
 
